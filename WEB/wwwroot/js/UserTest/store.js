77818b78a984b1df81cd582b2aa56bf447fbc55e
T480s <techcom.ec20@gmail.com>
2020-11-21 23:00:35 +0700

    Clean Code

--- WEB/wwwroot/js/UserTest/store.js
+++ WEB/wwwroot/js/UserTest/store.js
@@ -3,7 +3,6 @@ const store = Vue.reactive({});
 const inqueryModel = Vue.reactive({});
 const usertModel = Vue.reactive({});
 
-//================== Initial ==================
 function _initialModel(inquery, user) {
     Object.assign(inqueryModel, inquery)
     Object.assign(usertModel, user)
